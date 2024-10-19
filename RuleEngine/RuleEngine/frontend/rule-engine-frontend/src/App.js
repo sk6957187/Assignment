c750cde36634fbd58a0d05c4e742a83f505ac184
Sumit Kumar <sk6957187@gmail.com>
2024-10-20 01:02:56 +0530

    update

--- RuleEngine/RuleEngine/frontend/rule-engine-frontend/src/App.js
+++ RuleEngine/RuleEngine/frontend/rule-engine-frontend/src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 import RuleForm from './component/RuleForm';
+import RuleTake from './component/RuleTake';
 
-function App() {
-  return (
-    <div>
-      <RuleForm />
-    </div>
-  );
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        {/* <RuleTake /> */}
+        <RuleForm />
+      </>
+    );
+  }
 }
-
 export default App;
