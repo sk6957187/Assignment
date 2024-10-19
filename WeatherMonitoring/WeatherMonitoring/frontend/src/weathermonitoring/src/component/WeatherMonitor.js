56c51b698e43e0686c3309b1041d2789150c288b
Sumit Kumar <sk6957187@gmail.com>
2024-10-20 01:00:10 +0530

    update

--- WeatherMonitoring/WeatherMonitoring/frontend/src/weathermonitoring/src/component/WeatherMonitor.js
+++ WeatherMonitoring/WeatherMonitoring/frontend/src/weathermonitoring/src/component/WeatherMonitor.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -9,17 +8,15 @@ const WeatherMonitor = () => {
     const [threshold, setThreshold] = useState(35);
     const [alertMessage, setAlertMessage] = useState('');
 
-   
     const fetchCurrentWeather = async () => {
         try {
             const response = await axios.get(`http://localhost:8080/api/weather/current/${city}`);
             setWeather(response.data);
-            //console.log(response.data);
+            console.log(response.data);
         } catch (error) {
             console.error('Error fetching current weather:', error);
         }
     };
-
 
     const fetchDailySummary = async () => {
         try {
@@ -33,11 +30,11 @@ const WeatherMonitor = () => {
 
     const setTemperatureAlert = async () => {
         try {
-            const response = await axios.post(`http://localhost:8080/api/weather/set-alert/${city}`, null, {
-                params: { thresholdTemp: threshold },
+            const response = await axios.post(`http://localhost:8080/api/weather/set-alert/${city}`, {
+                thresholdTemp: Number(threshold),
             });
             setAlertMessage(response.data);
-            console.log(response.data);
+            console.log(`resp {} : ${response.data}`);
         } catch (error) {
             console.error('Error setting alert:', error);
         }
@@ -47,10 +44,8 @@ const WeatherMonitor = () => {
         fetchCurrentWeather();
         fetchDailySummary();
 
-       
         const interval = setInterval(fetchCurrentWeather, 300000); // 300000 ms = 5 minutes
 
-        // Clear the interval when the component unmounts or city changes
         return () => clearInterval(interval);
     }, [city]);
 
@@ -70,11 +65,15 @@ const WeatherMonitor = () => {
             </div>
 
             {weather && (
-                <div>
+                <div className="row">
                     <h2>Current Weather in {city}</h2>
-                    <p>Temperature: {weather.temp}°C</p>
-                    <p>Feels Like: {weather.feelsLike}°C</p>
+                    <p>Temperature: {weather.temp.toFixed(2)}°C</p>
+                    <p>Feels Like: {weather.feelsLike.toFixed(2)}°C</p>
                     <p>Condition: {weather.condition}</p>
+                    <p>Wind Speed: {weather.windSpeed} m/s</p>
+                    <p>Humidity: {weather.humidity}%</p>
+                    <p>Sea Level: {weather.seaLevel} hPa</p>
+                    <p>Visibility: {(weather.visibility / 1000).toFixed(2)} km</p>
                 </div>
             )}
 
