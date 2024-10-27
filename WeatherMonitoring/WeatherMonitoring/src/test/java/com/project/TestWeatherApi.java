5d931e7f407bf60da3578534f941c64ab3bd140b
Sumit Kumar <sk6957187@gmail.com>
2024-10-27 09:34:40 +0530

    update

--- WeatherMonitoring/WeatherMonitoring/src/test/java/com/project/TestWeatherApi.java
+++ WeatherMonitoring/WeatherMonitoring/src/test/java/com/project/TestWeatherApi.java
@@ -5,6 +5,7 @@ import com.project.service.WeatherService;
 import org.junit.Test;
 
 public class TestWeatherApi {
+
     public WeatherMonitoringConfiguration getAppConfig() {
         String configPath = "A:/workspace/Assignment/WeatherMonitoring/WeatherMonitoring/meta-data/app_env_config_test.yml";
         WeatherMonitoringConfiguration configuration = WeatherService.getAppConfig(configPath);
@@ -13,19 +14,26 @@ public class TestWeatherApi {
     @Test
     public void TestWeatherApi() {
         WeatherMonitoringConfiguration configuration = this.getAppConfig();
-        WeatherRepository weatherRepository = new WeatherRepository(configuration.getWeatherApiConfig(),configuration.getOracleSqlConfig());
+        WeatherRepository weatherRepository = new WeatherRepository(configuration.getWeatherApiConfig(),configuration.getOracleSqlConfig(), configuration.getUiConfig());
         weatherRepository.fetchWeather("DElhi");
+    }
+    @Test
+    public void getUiBaseApi() {
+        WeatherMonitoringConfiguration configuration = this.getAppConfig();
+        WeatherRepository weatherRepository = new WeatherRepository(configuration.getWeatherApiConfig(),configuration.getOracleSqlConfig(), configuration.getUiConfig());
+//        weatherRepository.getUiBaseApi();
+        System.out.println(weatherRepository.getUiBaseApi());
     }
     @Test
     public void TestWeatherData(){
         WeatherMonitoringConfiguration configuration = this.getAppConfig();
-        WeatherRepository weatherRepository = new WeatherRepository(configuration.getWeatherApiConfig(),configuration.getOracleSqlConfig());
+        WeatherRepository weatherRepository = new WeatherRepository(configuration.getWeatherApiConfig(),configuration.getOracleSqlConfig(), configuration.getUiConfig());
         weatherRepository.getDailySummary("DELHI");
     }
     @Test
     public void TestSetAlert(){
         WeatherMonitoringConfiguration configuration = this.getAppConfig();
-        WeatherRepository weatherRepository = new WeatherRepository(configuration.getWeatherApiConfig(),configuration.getOracleSqlConfig());
+        WeatherRepository weatherRepository = new WeatherRepository(configuration.getWeatherApiConfig(),configuration.getOracleSqlConfig(), configuration.getUiConfig());
         String rs = weatherRepository.setAlert("Delhi", 21);
         System.out.println(rs);
     }
