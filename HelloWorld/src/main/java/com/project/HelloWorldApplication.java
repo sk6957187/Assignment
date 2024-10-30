2b11c1a9fde1446d69f0358832ac6319f526bbd1
Sumit Kumar <sk6957187@gmail.com>
2024-10-30 20:59:09 +0530

    Added

--- HelloWorld/src/main/java/com/project/HelloWorldApplication.java
+++ HelloWorld/src/main/java/com/project/HelloWorldApplication.java
@@ -1,6 +1,7 @@
 package com.project;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.project.controller.HelloWorldResource;
 import io.dropwizard.Application;
 import io.dropwizard.assets.AssetsBundle;
@@ -10,8 +11,34 @@ import io.dropwizard.views.ViewBundle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+//    private static final Logger logger = LoggerFactory.getLogger(HelloWorldApplication.class);
+//
+//    @Override
+//    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+//        super.initialize(bootstrap);
+//        bootstrap.addBundle(new ViewBundle<>());
+//        bootstrap.addBundle(new AssetsBundle("/assets.libs/", "/assets.libs"));
+//    }
+//
+//    @Override
+//    public void run( HelloWorldConfiguration configuration, Environment environment) {
+//        logger.info("Application started: {}",configuration);
+//        HelloWorldResource resource = new HelloWorldResource(configuration);
+//        environment.jersey().register(resource);
+//    }
+//    public static void main(String[] args) throws Exception {
+//        new HelloWorldApplication().run(args);
+//    }
+//}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+
 public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
-    private static final Logger logger = LoggerFactory.getLogger(HelloWorldApplication.class);
+
+    final static Logger logger = LoggerFactory.getLogger(HelloWorldApplication.class);
+
+
 
     @Override
     public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
@@ -20,14 +47,13 @@ public class HelloWorldApplication extends Application<HelloWorldConfiguration> 
         bootstrap.addBundle(new AssetsBundle("/assets/", "/assets"));
     }
 
+
     @Override
-    public void run( HelloWorldConfiguration configuration, Environment environment) {
-        logger.info("Application started: {}",configuration);
-        HelloWorldResource resource = new HelloWorldResource(configuration);
-        environment.jersey().register(resource);
+    public void run(HelloWorldConfiguration configuration, Environment environment) {
+        logger.info("Application run success.");
+        environment.jersey().register(new HelloWorldResource(configuration));
     }
     public static void main(String[] args) throws Exception {
         new HelloWorldApplication().run(args);
     }
 }
-
