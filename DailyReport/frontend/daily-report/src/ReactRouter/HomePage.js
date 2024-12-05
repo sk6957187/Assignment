512320f9d35f3474d83d8d4ec066c1adfda2ddd3
Sumit Kumar <sk6957187@gmail.com>
2024-12-05 16:33:14 +0530

    update

---
+++ DailyReport/frontend/daily-report/src/ReactRouter/HomePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        This is Home page.
+      </div>
+    )
+  };
+}
+
+export default HomePage
