b438e7c4718769c1e4ae2bb47e178a5d6f47eaf3
Sumit Kumar <sk6957187@gmail.com>
2025-01-23 02:06:28 +0530

    add

---
+++ SQLProject/src/test/java/com/project/sql/sringboot/SqlProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sql.sringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
