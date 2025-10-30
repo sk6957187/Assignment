bd08ba02db9d0b937a2e83ba57c29254be3e5af7
Sumit Kumar <sk6957187@gmail.com>
2025-10-31 01:55:58 +0530

    Removed deleted BlogApplication files

--- BlogApplication/src/main/java/io/mountblue/BlogApplication/exception/NoDataFoundException.java
+++
@@ -1,8 +0,0 @@
-package io.mountblue.BlogApplication.exception;
-
-public class NoDataFoundException extends RuntimeException {
-
-    public NoDataFoundException(String mesg) {
-        super(mesg);
-    }
-}
