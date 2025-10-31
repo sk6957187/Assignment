e2e7667bc3385bb6a0dc629422d8e3af98c14ec2
Sumit Kumar <sk6957187@gmail.com>
2025-10-31 22:01:59 +0530

    update payment

--- foodiesapi/src/main/java/in/nayak/foodiesapi/service/OrderService.java
+++ foodiesapi/src/main/java/in/nayak/foodiesapi/service/OrderService.java
@@ -3,6 +3,19 @@ package in.nayak.foodiesapi.service;
 import in.nayak.foodiesapi.io.OrderRequest;
 import in.nayak.foodiesapi.io.OrderResponse;
 
+import java.util.List;
+import java.util.Map;
+
 public interface OrderService {
     OrderResponse createOrderWithPayment(OrderRequest request);
+
+    void verifyPayment(Map<String, String> paymentData, String Status);
+
+    List<OrderResponse> getUserOrders();
+
+    void removeOrder(String orderId);
+
+    List<OrderResponse> getOrderOfAllUsers();
+
+    void updateOrderStatus(String orderId, String status);
 }
