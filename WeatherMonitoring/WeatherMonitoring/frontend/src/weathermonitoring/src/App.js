faed29ddfd434052acd4424e94ab129fcb7bb10d
Sumit Kumar <sk6957187@gmail.com>
2025-11-07 21:04:54 +0530

    added google analytics

--- WeatherMonitoring/WeatherMonitoring/frontend/src/weathermonitoring/src/App.js
+++ WeatherMonitoring/WeatherMonitoring/frontend/src/weathermonitoring/src/App.js
@@ -1,6 +1,23 @@
+import { useEffect } from 'react';
 import WeatherMonitor from './component/WeatherMonitor';
+import ReactGA from "react-ga4";
+
 
 function App() {
+
+  useEffect(() => {
+    const GA_MEASUREMENT_ID = process.env.REACT_APP_GA_MEASUREMENT_ID;
+    if (GA_MEASUREMENT_ID) {
+      ReactGA.initialize(GA_MEASUREMENT_ID);
+      ReactGA.send({
+        hitType: "pageview",
+        page: window.location.pathname,
+        title: "App.jsx",
+      });
+    } else {
+      console.warn("Google Analytics ID not found in environment variables");
+    }
+  }, []);
   return (
     <div className="App">
       <div className = "container">
