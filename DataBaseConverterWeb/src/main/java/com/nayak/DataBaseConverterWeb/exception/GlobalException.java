a9867ca952da1602067d4530ad3e6cb235a962cb
Sumit Kumar <sk6957187@gmail.com>
2025-11-23 00:44:55 +0530

    added NoHandlerFoundException method

--- DataBaseConverterWeb/src/main/java/com/nayak/DataBaseConverterWeb/exception/GlobalException.java
+++ DataBaseConverterWeb/src/main/java/com/nayak/DataBaseConverterWeb/exception/GlobalException.java
@@ -1,14 +1,16 @@
 package com.nayak.DataBaseConverterWeb.exception;
 
 import com.nayak.DataBaseConverterWeb.entity.ResponseStructure;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
-public class GlobalException extends ResponseEntityExceptionHandler {
+public class GlobalException {
 
     @ExceptionHandler(DataNotFoundException.class)
     public ResponseEntity<ResponseStructure<String>> handleDataNotFound(DataNotFoundException exception){
@@ -17,5 +19,18 @@ public class GlobalException extends ResponseEntityExceptionHandler {
         resp.setMsg("Data not found..!!");
         resp.setData(exception.getMessage());
         return new ResponseEntity<>(resp, HttpStatus.OK);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public String handleNoHandlerFound(NoHandlerFoundException exception, HttpServletRequest request, Model model) {
+        int statusCode = 404;
+        String errorMessage = "Page Not Found";
+        
+        model.addAttribute("statusCode", statusCode);
+        model.addAttribute("errorMessage", errorMessage);
+        model.addAttribute("message", "The requested endpoint does not exist");
+        model.addAttribute("exception", exception);
+        
+        return "error";
     }
 }
