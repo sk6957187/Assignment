db498a03750aee81ee04ee330b0800ee614d3695
Sumit Kumar <sk6957187@gmail.com>
2026-06-27 22:41:05 +0530

    Added code for file and photo store in local device

---
+++ foodiesapi/src/main/java/in/nayak/foodiesapi/config/StaticResourceConfig.java
@@ -0,0 +1,20 @@
+package in.nayak.foodiesapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticResourceConfig
+        implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(
+            ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations(
+                        "file:///A:/workspace/Assignment/foodiesapi/metadata/image/"
+                );
+    }
+}
