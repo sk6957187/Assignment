498311e834019f544e900e58213138fdcb5ba33b
Sumit Kumar <sk6957187@gmail.com>
2026-07-17 00:29:42 +0530

    added repositery class

---
+++ oauth/src/main/java/com/nayak/oauth/repsitery/UserRepository.java
@@ -0,0 +1,14 @@
+package com.nayak.oauth.repsitery;
+
+import com.nayak.oauth.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, String> {
+
+    Optional<UserEntity> findByEmail(String email);
+
+}
